c25be175871e5ba7e0e402e7a6471890e22dd27b
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2021-03-21 22:40:13 +0100

    docker-compose: api services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
+
+interface Message {
+  text: string,
+  date: Date,
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'docker-app';
+  messages$ = this.http.get<[Message]>(environment.apiUrl + '/messages/');
+
+  constructor(private http: HttpClient) {
+  }
 }
